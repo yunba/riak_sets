b953fdda6225ac96806bf55df2dd2a6c49080b4e
Zachary Kessin <zkessin@gmail.com>
2015-04-16 08:21:09 +0300

    fromatting

--- src/riak_sets.erl
+++ src/riak_sets.erl
@@ -14,7 +14,7 @@
 
 %% @doc Pings a random vnode to make sure communication is functional
 ping() ->
-    DocIdx = riak_core_util:chash_key({<<"ping">>, term_to_binary(now())}),
-    PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, riak_sets),
+    DocIdx               = riak_core_util:chash_key({<<"ping">>, term_to_binary(now())}),
+    PrefList             = riak_core_apl:get_primary_apl(DocIdx, 1, riak_sets),
     [{IndexNode, _Type}] = PrefList,
     riak_core_vnode_master:sync_spawn_command(IndexNode, ping, riak_sets_vnode_master).
